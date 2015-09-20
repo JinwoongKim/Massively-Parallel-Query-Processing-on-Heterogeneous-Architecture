e412adfb75de5fbc5bd098da8b53631dd44bcaa2
JinwoongKim <aragnom@gmail.com>
2015-09-20 23:33:04 +0900

    Add the separate compilation example

---
+++ example/v3.h
@@ -0,0 +1,13 @@
+class v3
+{
+	public:
+		float x;
+		float y;
+		float z;
+
+		v3();
+		v3(float xIn, float yIn, float zIn);
+		void randomize();
+		__host__ __device__ void normalize();
+		__host__ __device__ void scramble();
+};
