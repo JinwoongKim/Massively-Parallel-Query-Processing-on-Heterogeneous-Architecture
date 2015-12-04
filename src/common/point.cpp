61c031892d5ff3ee1ee1cabafff83bb236d0018f
JinwoongKim <aragnom@gmail.com>
2015-12-05 07:02:50 +0900

    Update point class

--- src/common/point.cpp
+++ src/common/point.cpp
@@ -1,41 +1,63 @@
+#include "common/common.h"
 #include "common/point.h"
 
 #include <cassert>
 
 namespace ursus {
 
-unsigned int 
-Point::GetDims(void) const{ 
+unsigned int Point::GetDims(void) const{ 
   return number_of_dimensions; 
 }
 
-unsigned int 
-Point::GetBits(void) const{ 
+unsigned int Point::GetBits(void) const{ 
   return number_of_bits; 
 }
 
-unsigned long long* 
-Point::GetPoints(void) const{
+unsigned long long* Point::GetPoints(void) const{
   return points; 
 }
+unsigned long long GetPoint(unsigned int position) const;
+  assert(position >= 0 || position < GetDims());
+  return points[position]; 
+}
 
-void 
-Point::SetDims(unsigned int _number_of_dimensions) { 
+void Point::SetDims(unsigned int _number_of_dimensions) { 
   assert( _number_of_dimensions > 0 );
   number_of_dimensions = _number_of_dimensions; 
 }
 
-void 
-Point::SetBits(unsigned int _number_of_bits) { 
+void Point::SetBits(unsigned int _number_of_bits) { 
   assert( _number_of_bits > 0 );
   number_of_bits = _number_of_bits; 
 }
 
-void 
-Point::SetPoints(unsigned long long* _points) { 
-  //FIXME
-  //assert( _points != nullptr );
+void Point::SetPoints(unsigned long long* _points) { 
+  assert( _points != nullptr );
   points = _points; 
+}
+
+bool operator> (Point &p1, Point &p2) {
+  assert(p1.GetDims() == p2.GetDims());
+  unsigned int number_of_dimensions = p1.GetDims();
+
+ for(unsigned int range ( dim, 0, number_of_dimensions )) {
+   if( p1.GetPoint(dim) <= p2.GetPoint(dim))  {
+     return false;
+   }
+ }
+ return true;
+}
+     
+bool operator< (Point &p1, Point &p2) {
+  assert(p1.GetDims() == p2.GetDims());
+  unsigned int number_of_dimensions = p1.GetDims();
+
+ for(unsigned int range ( dim, 0, number_of_dimensions )) {
+   if( p1.GetPoint(dim) >= p2.GetPoint(dim))  {
+     return false;
+   }
+ }
+ return true;
 }
 
 } // End of ursus namespace
--- src/common/point.h
+++ src/common/point.h
@@ -3,40 +3,48 @@
 namespace ursus {
 
 class Point{
-  public:
-    Point(unsigned int number_of_dimensions,
-          unsigned int number_of_bits) 
-    : number_of_dimensions(number_of_dimensions),
-      number_of_bits(number_of_bits){
+ public:
+ //===--------------------------------------------------------------------===//
+ // Consteructor/Destructor
+ //===--------------------------------------------------------------------===//
+  Point(unsigned int number_of_dimensions,
+        unsigned int number_of_bits) 
+  : number_of_dimensions(number_of_dimensions),
+    number_of_bits(number_of_bits){
 
-      points = new unsigned long long [number_of_dimensions];
-    };
-    ~Point(){
-      delete points;
-    }
+    points = new unsigned long long [number_of_dimensions];
+  };
+  ~Point(){
+    delete points;
+  }
 
-    //===--------------------------------------------------------------------===//
-    // ACCESSORS
-    //===--------------------------------------------------------------------===//
+ //===--------------------------------------------------------------------===//
+ // ACCESSORS
+ //===--------------------------------------------------------------------===//
+  unsigned int GetDims(void) const;
+  unsigned int GetBits(void) const;
+  unsigned long long* GetPoints(void) const;
+  unsigned long long GetPoint(unsigned int position) const;
 
-    unsigned int GetDims(void) const;
+  void SetDims(unsigned int number_of_dimensions);
+  void SetBits(unsigned int number_of_bits);
+  void SetPoints(unsigned long long* points);
 
-    unsigned int GetBits(void) const;
+  friend bool operator> (Point &p1, Point &p2);
+  friend bool operator< (Point &p1, Point &p2);
 
-    unsigned long long* GetPoints(void) const;
+ //===--------------------------------------------------------------------===//
+ // Members
+ //===--------------------------------------------------------------------===//
+ private:
+  // # of dims
+  unsigned number_of_dimensions;
+  
+  // # of bits which represent each dimension 
+  unsigned number_of_bits;
 
-    void SetDims(unsigned int number_of_dimensions);
-
-    void SetBits(unsigned int number_of_bits);
-
-    void SetPoints(unsigned long long* points);
-
-  private:
-    unsigned number_of_dimensions;
-
-    unsigned number_of_bits;
-
-    unsigned long long* points;
+  // real point data 
+  unsigned long long* points;
 };
 
 } // End of ursus namespace
