ef2784869cf56cb671f3d8982e1d74bb0f94ddb4
JinwoongKim <aragnom@gmail.com>
2016-03-17 16:43:36 +0900

    Minor fix

--- generator/cuRAND.cpp
+++ generator/cuRAND.cpp
@@ -17,7 +17,7 @@
 int main(int argc, char *argv[]) { 
 
   if(argc<3){
-    printf("Usage: %s # data # dims\n", argv[0]);
+    printf("Usage: %s # data # dims (opt : offset)\n", argv[0]);
     exit(0);
   }
 
