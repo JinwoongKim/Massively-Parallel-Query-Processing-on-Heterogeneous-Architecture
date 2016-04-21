b4ec4b79fe74ae4a5e94d8d68369310bbe945fb7
JinwoongKim <aragnom@gmail.com>
2016-04-21 18:59:28 +0900

    Now transform in parallel

--- src/transformer/transformer.cpp
+++ src/transformer/transformer.cpp
@@ -5,9 +5,43 @@
 #include "evaluator/recorder.h"
 #include "node/branch.h"
 
+#include <thread>
 
 namespace ursus {
 namespace transformer {
+
+void Thread_Transform(node::Node *node, node::Node_SOA *node_soa,
+                      ui start_offset, ui end_offset) {
+
+  for(ui range(node_offset, start_offset, end_offset)) {
+    auto number_of_branches = node[node_offset].GetBranchCount();
+    node_soa[node_offset].SetBranchCount(number_of_branches);
+
+    for(ui range(branch_itr, 0, number_of_branches)) {
+      auto branch = node[node_offset].GetBranch(branch_itr);
+
+      auto points = branch.GetPoints();
+      auto index = branch.GetIndex();
+      auto child_offset = branch.GetChildOffset();
+
+      // set points in Node_SOA
+      for(ui range(dim_itr, 0, GetNumberOfDims()*2)) {
+        auto offset = dim_itr*GetNumberOfDegrees()+branch_itr;
+        node_soa[node_offset].SetPoint(offset, points[dim_itr]);
+      }
+
+      // set the index
+      node_soa[node_offset].SetIndex(branch_itr, index);
+      node_soa[node_offset].SetChildOffset(branch_itr, child_offset);
+    }
+
+    // node type 
+    node_soa[node_offset].SetNodeType(node[node_offset].GetNodeType());
+
+    // node level
+    node_soa[node_offset].SetLevel(node[node_offset].GetLevel());
+  }
+}
 
 /**
  * @brief Transform the Node into Node_SOA
@@ -22,38 +56,33 @@ node::Node_SOA* Transformer::Transform(node::Node* node,
 
   node::Node_SOA* node_soa = new node::Node_SOA[number_of_nodes];
 
-  for(ui range(node_itr, 0, number_of_nodes)) {
-    auto number_of_branches = node[node_itr].GetBranchCount();
-    node_soa[node_itr].SetBranchCount(number_of_branches);
+  const size_t number_of_threads = std::thread::hardware_concurrency();
 
-    for(ui range(branch_itr, 0, number_of_branches)) {
-      auto branch = node[node_itr].GetBranch(branch_itr);
+  // parallel for loop using c++ std 11 
+  {
+    std::vector<std::thread> threads;
 
-      auto points = branch.GetPoints();
-      auto index = branch.GetIndex();
-      auto child_offset = branch.GetChildOffset();
+    auto chunk_size = number_of_nodes/number_of_threads;
+    auto start_offset = 0 ;
+    auto end_offset = start_offset + chunk_size + number_of_nodes%number_of_threads;
+      
+    //Launch a group of threads
+    for (ui range(thread_itr, 0, number_of_threads)) {
+      threads.push_back(std::thread(Thread_Transform, std::ref(node), 
+                        std::ref(node_soa), start_offset, end_offset));
 
-
-      // set points in Node_SOA
-      for(ui range(dim_itr, 0, GetNumberOfDims()*2)) {
-        auto offset = dim_itr*GetNumberOfDegrees()+branch_itr;
-        node_soa[node_itr].SetPoint(offset, points[dim_itr]);
-      }
-
-      // set the index
-      node_soa[node_itr].SetIndex(branch_itr, index);
-      node_soa[node_itr].SetChildOffset(branch_itr, child_offset);
+      start_offset = end_offset;
+      end_offset += chunk_size;
     }
 
-    // node type 
-    node_soa[node_itr].SetNodeType(node[node_itr].GetNodeType());
-
-    // node level
-    node_soa[node_itr].SetLevel(node[node_itr].GetLevel());
+    //Join the threads with the main thread
+    for(auto &thread : threads){
+      thread.join();
+    }
   }
 
   auto elapsed_time = recorder.TimeRecordEnd();
-  LOG_INFO("Transform Time on the GPU = %.6fs", elapsed_time/1000.0f);
+  LOG_INFO("Transform Time on the CPU = %.6fs", elapsed_time/1000.0f);
 
   return node_soa;
 }
